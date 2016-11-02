f361ac4fdec1441f1a288401dcc01c874e24219a
MargaritaShum <lalayt666@gmail.com>
2016-11-02 10:01:03 +0200

    changes for 01.11.2016

--- client/app/face/faceCtrl.js
+++ client/app/face/faceCtrl.js
@@ -5,24 +5,51 @@
 
     function faceCtrl() {
         var vm = this;
-        vm.faces = [];
+        vm.items = [];
+        vm.calcK = calcK;
         activate();
 
         function activate() {
-            vm.faces = [{
-            	id:1,
-            	fruit:'Apple', 
-            	kkal:10,
-            	date: new Date(2015, 10, 16)
-            },
-            {
-            	id:2,
-            	fruit:'Strawsberry',
-            	kkal:20,
-            	date: new Date(2015, 10, 16)
-            }
+            vm.items = [{
+                date: new Date(2015, 10, 16),
+                comeMeals: [{
+                        fruit: 'Strawsberry',
+                        kkal: 20,
+                        time: '18:00',
+                        junkFood:true
+                    }, {
+                        fruit: 'Apple',
+                        kkal: 10,
+                        time: '20:00',
+                        junkFood:false
+                    }
+                ]
+            }, {
+                date: new Date(2016, 11, 20),
+                comeMeals: [{
+                        fruit: 'Pineapple',
+                        kkal: 20,
+                        time: '07:00',
+                        junkFood:false
+                    }, {
+                        fruit: 'Fish',
+                        kkal: 10,
+                        time: '10:00',
+                        junkFood:true
+                    }
+                ]
+            }];
+        }
 
-            ];
+        function calcK(item, flag) {
+            var sum = 0;
+            for (var i = 0, l = item.comeMeals.length; i < l; i += 1) {
+                var cm = item.comeMeals[i];
+                if (cm.junkFood) {
+                    sum += cm.kkal;
+                }
+            }
+            return sum;
         }
     }
 
